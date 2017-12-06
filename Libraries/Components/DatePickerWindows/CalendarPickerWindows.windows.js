b5b9b0a72bc867ac308d807f1c593d33d2ac9e3b
Tom Van Boghout <tom.vanboghout@rombit.be>
2017-12-06 21:00:09 +0100

    Added CalendarPicker

--- Libraries/Components/DatePickerWindows/CalendarPickerWindows.windows.js
+++ Libraries/Components/DatePickerWindows/CalendarPickerWindows.windows.js
@@ -11,88 +11,114 @@
  */
 'use strict';
 
+var PropTypes = require('prop-types');
 var React = require('React');
 var View = require('View');
 var requireNativeComponent = require('requireNativeComponent');
-
-var ReactNative = require("react-native");
-
-var CalendarPickerWindows = React.createClass({
-  name: 'CalendarPickerWindows',
-  propTypes: {
-    ...View.PropTypes,
-    ...ReactNative.ViewPropTypes,
-
-    /**
-     * The currently selected date.
-     */
-    date: React.PropTypes.instanceOf(Date),
-    /**
-     * Date change handler.
-     *
-     * This is called when the user changes the date or time in the UI.
-     * The first and only argument is a Date object representing the new
-     * date.
-     */
-    onChange: React.PropTypes.func,
-
-    /**
-     * Minimum date.
-     *
-     * Restricts the range with an lower bound.
-     */
-    minDate: React.PropTypes.instanceOf(Date),
-
-    /**
-     * Maximum date.
-     *
-     * Restricts the range with an upper bound.
-     */
-    maxDate: React.PropTypes.instanceOf(Date),
-
-    /**
-     * Placeholder text for the component.
-     */
-    placeholderText: React.PropTypes.string,
-
-    /**
-     * Calendar type, defined as a string.
-     *
-     * The supported values are defined in the Windows.Globalization.CalendarIdentifiers class.
-     * Some values require API v10.0.14393.0 or above
-     */
-    calendarIdentifier: React.PropTypes.oneOf('ChineseLunarCalendar', 'GregorianCalendar', 'HebrewCalendar',
-      'HijriCalendar', 'JapaneseCalendar', 'JapaneseLunarCalendar',
-      'JulianCalendar', 'KoreanCalendar', 'KoreanLunarCalendar',
-      'PersianCalendar', 'TaiwanCalendar', 'TaiwanLunarCalendar',
-      'ThaiCalendar', 'UmAlQuraCalendar', 'VietnameseLunarCalendar'),
-    /**
-     * First day of the week, defined as an integer with 0 being Sunday, 1 Monday, etc.
-     */
-    firstDayOfWeek: React.PropTypes.oneOf(0, 1, 2, 3, 4, 5, 6),
-  },
-  getDefaultProps: function() {
-    return {
-      firstDayOfWeek: 0,
-      calendarIdentifier: 'GregorianCalendar',
-    };
-  },
-  _onChange: function(event) {
-    this.props.onChange && this.props.onChange(new Date(event.nativeEvent.date));
-  },
-  render: function() {
-    return <RCTCalendarDatePicker
-      style={this.props.style}
-      date={this.props.date}
-      minDate={this.props.minDate}
-      maxDate={this.props.maxDate}
-      onChange={this._onChange}
-      placeholderText={this.props.placeholderText}
-      calendarIdentifier={this.props.calendarIdentifier}
-      firstDayOfWeek={this.props.firstDayOfWeek} />
-  },
-});
-
 var RCTCalendarDatePicker = requireNativeComponent('RCTCalendarDatePicker', CalendarPickerWindows);
+var ReactNative = require("react-native")
+
+class CalendarPickerWindows extends React.Component {
+  props: {
+    date: Date,
+    onchange?: Function,
+    // minDate?: Date,
+    // maxDate?: Date,
+    firstDayOfWeek: number,
+  };
+
+  _onChange = (event) => this.props.onChange && this.props.onChange(new Date(event.nativeEvent.date));
+
+  render() {
+    return (
+      <RCTCalendarDatePicker
+        // style={this.props.style}
+        date={this.props.date}
+        // minDate={this.props.minDate}
+        // maxDate={this.props.maxDate}
+        onChange={this._onChange}
+        // placeholderText={this.props.placeholderText}
+        calendarIdentifier={'GregorianCalendar'}
+        firstDayOfWeek={1}
+      />
+    );
+  }
+}
+
+// var CalendarPickerWindows = React.createClass({
+//   name: 'CalendarPickerWindows',
+//   propTypes: {
+//     ...View.PropTypes,
+//     ...ReactNative.ViewPropTypes,
+//
+//     /**
+//      * The currently selected date.
+//      */
+//     date: React.PropTypes.instanceOf(Date),
+//     /**
+//      * Date change handler.
+//      *
+//      * This is called when the user changes the date or time in the UI.
+//      * The first and only argument is a Date object representing the new
+//      * date.
+//      */
+//     onChange: React.PropTypes.func,
+//
+//     /**
+//      * Minimum date.
+//      *
+//      * Restricts the range with an lower bound.
+//      */
+//     minDate: React.PropTypes.instanceOf(Date),
+//
+//     /**
+//      * Maximum date.
+//      *
+//      * Restricts the range with an upper bound.
+//      */
+//     maxDate: React.PropTypes.instanceOf(Date),
+//
+//     /**
+//      * Placeholder text for the component.
+//      */
+//     placeholderText: React.PropTypes.string,
+//
+//     /**
+//      * Calendar type, defined as a string.
+//      *
+//      * The supported values are defined in the Windows.Globalization.CalendarIdentifiers class.
+//      * Some values require API v10.0.14393.0 or above
+//      */
+//     calendarIdentifier: React.PropTypes.oneOf('ChineseLunarCalendar', 'GregorianCalendar', 'HebrewCalendar',
+//       'HijriCalendar', 'JapaneseCalendar', 'JapaneseLunarCalendar',
+//       'JulianCalendar', 'KoreanCalendar', 'KoreanLunarCalendar',
+//       'PersianCalendar', 'TaiwanCalendar', 'TaiwanLunarCalendar',
+//       'ThaiCalendar', 'UmAlQuraCalendar', 'VietnameseLunarCalendar'),
+//     /**
+//      * First day of the week, defined as an integer with 0 being Sunday, 1 Monday, etc.
+//      */
+//     firstDayOfWeek: React.PropTypes.oneOf(0, 1, 2, 3, 4, 5, 6),
+//   },
+//   getDefaultProps: function() {
+//     return {
+//       firstDayOfWeek: 0,
+//       calendarIdentifier: 'GregorianCalendar',
+//     };
+//   },
+//   _onChange: function(event) {
+//     this.props.onChange && this.props.onChange(new Date(event.nativeEvent.date));
+//   },
+//   render: function() {
+//     return <RCTCalendarDatePicker
+//       style={this.props.style}
+//       date={this.props.date}
+//       minDate={this.props.minDate}
+//       maxDate={this.props.maxDate}
+//       onChange={this._onChange}
+//       // placeholderText={this.props.placeholderText}
+//       calendarIdentifier={'GregorianCalendar'}
+//       firstDayOfWeek={0} />
+//   },
+// });
 
 module.exports = CalendarPickerWindows;
